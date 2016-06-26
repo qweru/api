c06ebc89b674a5189ec11bbf1f9f3854c93c6298
TelegramDev <makan.j110110@gmail.com>
2016-06-27 04:03:45 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -232,32 +232,28 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {90285047},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v4
+    about_text = [[SudoRobot v1
 An advanced administration bot based on TG-CLI written in Lua
 
-https://github.com/SEEDTEAM/TeleSeed
 
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@POTUS [Developer]
-@seyedan25 [Manager]
-@aRandomStranger [Admin]
+SUDO:
+@MAKAN
 
 Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
+Seed team
+Amir
+Reza
+Dilan
+Parham
+Arshia
 
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+Our channel
+@MoonTeam
 
 Our website 
-http://teleseed.seedteam.org/
+Comming soon...
 ]],
     help_text_realm = [[
 Realm Commands:
