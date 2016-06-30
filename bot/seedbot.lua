a3b0e8c8801c6df57b7e4435f7276113c5212e5f
qweru <sara01dilan@gmail.com>
2016-06-30 12:24:53 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {213520858,0,tonumber(our_id)},--Sudo users
+    sudo_users = {213520858},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
